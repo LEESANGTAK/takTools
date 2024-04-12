5cdca414d771ecf4bd66d200128dc62d21e9bd83
Tak <chst27@gmail.com>
2024-04-12 14:09:16 +0900

    added preferences files

---
+++ bat/Tak_Maya 2019.bat
@@ -0,0 +1,2 @@
+set MAYA_APP_DIR=C:\GoogleDrive\programs_env\maya
+start "" "C:\Program Files\Autodesk\Maya2019\bin\maya.exe"
---
+++ templates/moduleTemplate/scripts/moduleTemplate/__init__.py
@@ -0,0 +1,4 @@
+from imp import reload
+
+from . import api; reload(api)
+from . import gui; reload(gui)
