04940935ebdf86a68101c592ee91e5bac93f5dd1
Tak <chst27@gmail.com>
2024-08-16 19:57:33 +0900

    rename a batch file for launching maya with arbitary app dir

---
+++ bat/Custom_Maya_APP_DIR.bat
@@ -0,0 +1,2 @@
+set MAYA_APP_DIR=c:\Users\chst2\Documents\maya
+start "" "C:\Program Files\Autodesk\Maya2020\bin\maya.exe"
--- bat/Tak_Maya 2019.bat
+++
@@ -1,2 +0,0 @@
-set MAYA_APP_DIR=C:\GoogleDrive\programs_env\maya
-start "" "C:\Program Files\Autodesk\Maya2019\bin\maya.exe"
