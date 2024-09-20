2367b07b474dce8465a4e6c28ed381bd1ea08831
Tak <chst27@gmail.com>
2024-09-20 16:46:11 +0900

    clean up icons

--- scripts/takTools/common/iconMaker.py
+++ scripts/takTools/common/iconMaker.py
@@ -17,20 +17,30 @@ class IconMakerGUI(object):
         self.__createCaptureCam()
         self.__connect()
         pm.setAttr("hardwareRenderingGlobals.multiSampleEnable", True)
+        curLineWidth = pm.displayPref(q=True, lineWidth=True)
+        pm.floatSliderGrp(self.__lineWidthSlider, e=True, v=curLineWidth)
 
     def show(self):
         pm.showWindow(self.__win)
         self.__fitAllObjects()
 
     def __build(self):
+        if pm.modelEditor('captureModelEditor', exists=True):
+            cmds.deleteUI('captureModelEditor')
+
         self.__win = pm.window(title='Icon Maker GUI', mnb=False, mxb=False)
 
         mainFormLayout = pm.formLayout()
 
-        buttonLayout = pm.rowColumnLayout(numberOfColumns=5)
-        self.__sizeIntFldGrp = pm.intFieldGrp(label='Size: ', v1=64, columnWidth=[(1, 30), (2, 30)])
+        buttonLayout = pm.rowColumnLayout(numberOfColumns=6)
+        self.__sizeOptMenuGrp = pm.optionMenuGrp(label='Size: ', columnWidth=[(1, 30), (2, 30)])
+        pm.menuItem(label='32')
+        pm.menuItem(label='64')
+        pm.menuItem(label='128')
+        pm.optionMenuGrp(self.__sizeOptMenuGrp, e=True, v='64')
         self.__lineWidthSlider = pm.floatSliderGrp(label='Line Width: ', min=1.0, max=10.0, columnWidth=[(1, 70), (2, 70)])
         self.__wireBtn = pm.symbolButton(image='WireFrame.png')
+        self.__wireShadeBtn = pm.symbolButton(image='WireFrameOnShaded.png')
         self.__textureBtn = pm.symbolButton(image='Textured.png')
         self.__fitBtn = pm.symbolButton(image='zoom.png')
 
@@ -38,11 +48,11 @@ class IconMakerGUI(object):
         self.__modelEditor = pm.modelEditor('captureModelEditor')
         pm.modelEditor(self.__modelEditor, e=True, hud=False)
         pm.modelEditor(self.__modelEditor, e=True, grid=False)
-        pm.modelEditor('{}'.format(self.__modelEditor), e=True, displayTextures=False)
+        pm.modelEditor(self.__modelEditor, e=True, displayTextures=False)
         pm.modelEditor(self.__modelEditor, e=True, displayAppearance='smoothShaded')
         pm.modelEditor(self.__modelEditor, edit=True, jointXray=True)
 
-        captureLayout = pm.rowColumnLayout(numberOfColumns=3, columnWidth=[(1, 30), (2, 230), (3, 38)])
+        captureLayout = pm.rowColumnLayout(numberOfColumns=3, columnWidth=[(1, 30), (2, 260), (3, 38)])
         self.__iconPathBtn = pm.symbolButton(image='fileOpen.png')
         self.__filePathFld = pm.textField(text=IconMakerGUI.DEFAULT_ICON_PATH)
         self.__captureBtn = pm.symbolButton(image='UVEditorSnapshot.png')
@@ -58,7 +68,7 @@ class IconMakerGUI(object):
             ]
         )
 
-        pm.window(self.__win, e=True, w=100, h=363, sizeable=False)
+        pm.window(self.__win, e=True, w=100, h=394, sizeable=False)
 
     def __createCaptureCam(self):
         self.__capCam = pm.camera(n='captureCam')[0]
@@ -72,6 +82,7 @@ class IconMakerGUI(object):
         pm.window(self.__win, e=True, closeCommand=self.__closeCallback)
         pm.floatSliderGrp(self.__lineWidthSlider, e=True, dragCommand=self.__setLineWidth)
         pm.symbolButton(self.__wireBtn, e=True, command=self.__toggleWireframe)
+        pm.symbolButton(self.__wireShadeBtn, e=True, command=self.__toggleWireframeShade)
         pm.symbolButton(self.__textureBtn, e=True, command=self.__toggleDisplayTexture)
         pm.symbolButton(self.__fitBtn, e=True, command=self.__fitAllObjects)
         pm.symbolButton(self.__iconPathBtn, e=True, command=self.__getFilePath)
@@ -82,32 +93,34 @@ class IconMakerGUI(object):
 
     def __setLineWidth(self, *args):
         width = pm.floatSliderGrp(self.__lineWidthSlider, q=True, value=True)
-        pm.modelEditor('{}'.format(self.__modelEditor), e=True, lineWidth=width)
+        pm.modelEditor(self.__modelEditor, e=True, lineWidth=width)
 
     def __toggleWireframe(self, *args):
-        curDisplayAppearance = pm.modelEditor('{}'.format(self.__modelEditor), q=True, displayAppearance=True)
+        curDisplayAppearance = pm.modelEditor(self.__modelEditor, q=True, displayAppearance=True)
         if curDisplayAppearance == 'wireframe':
-            pm.modelEditor('{}'.format(self.__modelEditor), e=True, displayAppearance='smoothShaded')
+            pm.modelEditor(self.__modelEditor, e=True, displayAppearance='smoothShaded')
             pm.symbolButton(self.__wireBtn, e=True, bgc=(0.267, 0.267, 0.267))
         elif curDisplayAppearance == 'smoothShaded':
-            pm.modelEditor('{}'.format(self.__modelEditor), e=True, displayAppearance='wireframe')
+            pm.modelEditor(self.__modelEditor, e=True, displayAppearance='wireframe')
             pm.symbolButton(self.__wireBtn, e=True, bgc=(0.322, 0.522, 0.651))
 
-    def __toggleDisplayTexture(self, *args):
-        curDisplayAppearance = pm.modelEditor('{}'.format(self.__modelEditor), q=True, displayAppearance=True)
-        if curDisplayAppearance != 'smoothShaded':
-            pm.modelEditor('{}'.format(self.__modelEditor), e=True, displayAppearance='smoothShaded')
-            pm.modelEditor('{}'.format(self.__modelEditor), e=True, displayTextures=True)
-            pm.symbolButton(self.__wireBtn, e=True, bgc=(0.267, 0.267, 0.267))
+    def __toggleWireframeShade(self, *args):
+        wireframeOnShaded = pm.modelEditor(self.__modelEditor, q=True, wireframeOnShaded=True)
+        if wireframeOnShaded:
+            pm.modelEditor(self.__modelEditor, e=True, wireframeOnShaded=False)
+            pm.symbolButton(self.__wireShadeBtn, e=True, bgc=(0.267, 0.267, 0.267))
         else:
-            curTextureState = pm.modelEditor('{}'.format(self.__modelEditor), q=True, displayTextures=True)
-            pm.modelEditor('{}'.format(self.__modelEditor), e=True, displayTextures=not curTextureState)
+            pm.modelEditor(self.__modelEditor, e=True, wireframeOnShaded=True)
+            pm.symbolButton(self.__wireShadeBtn, e=True, bgc=(0.322, 0.522, 0.651))
 
-        isDisplayTexture = pm.modelEditor('{}'.format(self.__modelEditor), q=True, displayTextures=True)
-        if isDisplayTexture:
-            pm.symbolButton(self.__textureBtn, e=True, bgc=(0.322, 0.522, 0.651))
-        else:
+    def __toggleDisplayTexture(self, *args):
+        displayTextures = pm.modelEditor(self.__modelEditor, q=True, displayTextures=True)
+        if displayTextures:
+            pm.modelEditor(self.__modelEditor, e=True, displayTextures=False)
             pm.symbolButton(self.__textureBtn, e=True, bgc=(0.267, 0.267, 0.267))
+        else:
+            pm.modelEditor(self.__modelEditor, e=True, displayTextures=True)
+            pm.symbolButton(self.__textureBtn, e=True, bgc=(0.322, 0.522, 0.651))
 
     def __fitAllObjects(self, *args):
         pm.modelEditor(self.__modelEditor, e=True, activeView=True)
@@ -122,7 +135,7 @@ class IconMakerGUI(object):
     def __captureViewport(self, *args):
         pm.setFocus('modelPanel4')  # This is a tricky part
 
-        iconSize = pm.intFieldGrp(self.__sizeIntFldGrp, q=True, v1=True)
+        iconSize = int(pm.optionMenuGrp(self.__sizeOptMenuGrp, q=True, v=True))
         iconPath = pm.textField(self.__filePathFld, q=True, text=True)
         ext = os.path.splitext(iconPath)[-1].strip('.')
 
