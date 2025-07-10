a84eebdcb938888dda1ca65bd273b1112ae9c671
Tak <chst27@gmail.com>
2025-07-10 11:13:55 +0900

    Add normalizeCardUVs function to handle UV normalization for card meshes

--- scripts/takTools/utils/uv.py
+++ scripts/takTools/utils/uv.py
@@ -1,6 +1,8 @@
+import pymel.core as pm
 import maya.cmds as cmds
 
 from . import globalUtil
+from . import mesh as meshUtil
 
 
 def replaceAllUVSets(source, target):
@@ -54,3 +56,42 @@ def createLightMapUVSet(lightMapUVSetName='LightMapUV'):
         pm.polyUVSet(sel.getShape(), currentUVSet=True, uvSet=allUVSets[0])
     if cleanupNeedObjs:
         pm.warning('{} are need to clean up UV Sets.'.format(cleanupNeedObjs))
+
+
+def normalizeCardUVs(selections):
+    # Get a mesh and dupMesh from selections
+    mesh = None
+    dupMesh = None
+    faces = cmds.filterExpand(selections, sm=34)
+    if faces:
+        mesh = [cmds.listRelatives(shape, p=True)[0] for shape in list(set(cmds.ls(faces, objectsOnly=True)))][0]
+        dupMesh = meshUtil.duplicateFace(faces)
+    else:
+        mesh = selections[0]
+        dupMesh = cmds.duplicate(mesh)[0]
+
+    # Normalize uvs for separated card meshes
+    cards = cmds.filterExpand(cmds.polySeparate(dupMesh), sm=12)
+    for card in cards:
+        cmds.select(card, r=True)
+        cmds.polyNormalizeUV(normalizeType=1, preserveAspectRatio=False, centerOnTile=False)
+    normalizedUVsMesh = cmds.filterExpand(cmds.polyUnite(dupMesh), sm=12)[0]
+
+    # Transfer uvs from separated card meshes to original mesh
+    target = faces or mesh
+    cmds.transferAttributes(normalizedUVsMesh, target,
+        sampleSpace=0,
+        transferPositions=False,
+        transferNormals=False,
+        transferColors=False,
+        transferUVs=True,
+        searchMethod=3
+    )
+
+    # Clean up
+    cmds.delete(mesh, normalizedUVsMesh, ch=True)
+    cmds.delete(normalizedUVsMesh)
+    if cmds.objExists(dupMesh):
+        cmds.delete(dupMesh)
+
+    cmds.select(mesh, r=True)
